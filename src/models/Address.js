64bf2f4b3141e00d2b8b059634cccc46c16b4e8a
Matheus Lima <matheeus161@gmail.com>
2023-05-26 18:33:04 -0300

    feat: add Institution Model and refactor Address Model

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,43 +1,44 @@
 import { Schema } from 'mongoose';
 import Joi from 'joi';
 
-const Address = new Schema(
-    {
-        cep: {
-            type: String,
-            required: true
-        },
-
-        street: {
-            type: String,
-            required: true
-        },
-
-        neighborhood: {
-            type: String,
-            required: true
-        },
-
-        number: {
-            type: Number,
-            required: true
-        },
-
-        additionalInfo: {
-            type: String,
-            required: false
-        }
-    }
-);
+const Address = new Schema({
+    cep: {
+        type: String,
+        required: true,
+    },
+    street: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    additionalInfo: {
+        type: String,
+        required: false,
+    },
+});
 
 const addressRules = Joi.object({
     // O eslint diz que podemos remover a contrabarra. Será mesmo? Na dúvida, desabilitei o warning.
     // eslint-disable-next-line no-useless-escape
-    cep: Joi.string().pattern(new RegExp(/^\d{5}-\d{3}$/)).required(),
+    cep: Joi.string()
+        .pattern(new RegExp(/^\d{5}-\d{3}$/))
+        .required(),
     street: Joi.string().max(50).required(),
-    neighborhood: Joi.string().max(50).required(),
+    city: Joi.string().max(50).required(),
+    country: Joi.string().max(50).required(),
     number: Joi.number().integer().positive().required(),
-    additionalInfo: Joi.string().max(100)
+    additionalInfo: Joi.string().max(100),
 });
 
 export { Address, addressRules };
---
+++ src/models/Institution.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import Joi from 'joi';
+import { Address, addressRules } from './Address';
+
+const InstitutionSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: Address,
+            required: true,
+        },
+        information: {
+            type: String,
+            required: false,
+        },
+        events: {
+            type: String,
+            required: false,
+        },
+    },
+    { timeStamps: true, discriminatorKey: 'role' }
+);
+
+const Institution = model('Institution', InstitutionSchema);
+
+const intitutionRules = Joi.object({
+    name: Joi.string().max(100).required(),
+    address: addressRules.required(),
+    information: Joi.string().max(100),
+    events: Joi.string().max(100),
+});
+
+export { Institution, intitutionRules };
