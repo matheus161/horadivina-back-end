f5cbd7bab025dd5f2dc8234aeeca30dc562ee4d1
Matheus Lima <matheeus161@gmail.com>
2023-06-28 14:22:05 -0300

    fiz route login restriction

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,27 +1,27 @@
-import { Router } from "express";
-import emailInUse from "../middlewares/emailInUse";
-import limitRequests from "../middlewares/limitRequests";
-import validate from "../middlewares/validate";
-import SessionController from "../controllers/SessionController";
-import { authRules } from "../models/User";
-import { adminAuthRules } from "../models/Admin";
+import { Router } from 'express';
+import emailInUse from '../middlewares/emailInUse';
+import limitRequests from '../middlewares/limitRequests';
+import validate from '../middlewares/validate';
+import SessionController from '../controllers/SessionController';
+import { authRules } from '../models/User';
+import { adminAuthRules } from '../models/Admin';
 
 const router = Router();
 
 router.post(
-  "/",
-  limitRequests.heavily,
-  validate(authRules),
-  emailInUse,
-  SessionController.auth
+    '/',
+    limitRequests.slightly,
+    validate(authRules),
+    emailInUse,
+    SessionController.auth
 );
 
 router.post(
-  "/admin",
-  limitRequests.heavily,
-  validate(adminAuthRules),
-  emailInUse,
-  SessionController.authAdmin
+    '/admin',
+    limitRequests.slightly,
+    validate(adminAuthRules),
+    emailInUse,
+    SessionController.authAdmin
 );
 
-export default { router, name: "/auth" };
+export default { router, name: '/auth' };
