24958b6f574494d010b900eed693ce61fce989aa
Matheus Lima <matheeus161@gmail.com>
2023-07-01 09:32:28 -0300

    some religion refactors

--- src/models/Religion.js
+++ src/models/Religion.js
@@ -1,38 +1,28 @@
-import { Schema, model } from "mongoose";
-import Joi from "joi";
+import { Schema, model } from 'mongoose';
+import Joi from 'joi';
 
 const ReligionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+            required: true,
+        },
     },
-    origin: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    resume: {
-      type: String,
-    },
-  },
 
-  { timeStamps: true, discriminatorKey: "role" }
+    { timeStamps: true, discriminatorKey: 'role' }
 );
 
-const Religion = model("Religion", ReligionSchema);
+const Religion = model('Religion', ReligionSchema);
 
 const religionRules = Joi.object({
-  name: Joi.string()
-    .pattern(new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/))
-    .required(),
-  origin: Joi.string().pattern(new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/)),
-  description: Joi.string().pattern(
-    new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/)
-  ),
-  resume: Joi.string().pattern(new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/)),
+    name: Joi.string()
+        .pattern(new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/))
+        .required(),
+    avatar: Joi.string().pattern(new RegExp(/^[A-Za-zÁÉÍÓÚáéíóúãõÃÕâêôÂÊÔ ]+$/)),
 });
 
 export { Religion, religionRules };
--- src/routes/religionRoute.js
+++ src/routes/religionRoute.js
@@ -1,15 +1,16 @@
-import { Router } from "express";
-import ReligionController from "../controllers/ReligionController";
-import limitRequests from "../middlewares/limitRequests";
-import verifyId from "../middlewares/verifyId";
+import { Router } from 'express';
+import ReligionController from '../controllers/ReligionController';
+import limitRequests from '../middlewares/limitRequests';
+import verifyId from '../middlewares/verifyId';
+import verifyTokenAdmin from '../middlewares/verifyTokenAdmin';
 
 const router = Router();
 
 router.use(limitRequests.slightly);
 
-router.post("/", ReligionController.create);
-router.put("/:id", verifyId, ReligionController.update);
-router.get("/", ReligionController.getAll);
-router.delete("/:id", verifyId, ReligionController.remove);
+router.post('/', verifyTokenAdmin, ReligionController.create);
+router.put('/:id', verifyId, ReligionController.update);
+router.get('/', ReligionController.getAll);
+router.delete('/:id', verifyTokenAdmin, verifyId, ReligionController.remove);
 
-export default { router, name: "/religion" };
+export default { router, name: '/religion' };
