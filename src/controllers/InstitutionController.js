345fb198f42f174626be8461205d7ba80f50dbda
Matheus Lima <matheeus161@gmail.com>
2023-07-04 13:08:51 -0300

    refactor institution

--- src/controllers/InstitutionController.js
+++ src/controllers/InstitutionController.js
@@ -1,88 +1,106 @@
-import { Institution } from '../models/Institution';
+import { Institution } from "../models/Institution";
 
 async function create(req, res) {
-    try {
-        const {
-            name, information, address, dailyEvents
-        } = req.body;
+  try {
+    const { name, manager, information, address, dailyEvents } = req.body;
 
-        const institution = await Institution.create({
-            name,
-            address,
-            information,
-            dailyEvents,
-        });
+    const institution = await Institution.create({
+      name,
+      manager,
+      address,
+      information,
+      dailyEvents,
+    });
 
-        return res.status(201).json(institution);
-    } catch ({ message }) {
-        return res.status(500).json({ message });
-    }
+    return res.status(201).json(institution);
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
 }
 
 async function update(req, res) {
-    try {
-        const institution = await Institution.findById(req.params.id);
+  try {
+    const institution = await Institution.findById(req.params.id);
 
-        if (!institution) {
-            return res.status(404).json({
-                message: 'Instituição não encontrada',
-            });
-        }
-
-        const institutionUpdated = await Institution.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-        );
-
-        return res.status(200).json(institutionUpdated);
-    } catch ({ message }) {
-        return res.status(500).json({ message });
+    if (!institution) {
+      return res.status(404).json({
+        message: "Instituição não encontrada",
+      });
     }
+
+    const institutionUpdated = await Institution.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    return res.status(200).json(institutionUpdated);
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
 }
 
 async function remove(req, res) {
-    try {
-        const institution = await Institution.findByIdAndRemove(req.params.id);
+  try {
+    const institution = await Institution.findByIdAndRemove(req.params.id);
 
-        if (!institution) {
-            return res.status(404).json({ message: 'Instituição não encontrada.' });
-        }
-
-        return res
-            .status(200)
-            .json({ message: 'Instituição deletada com sucesso' });
-    } catch ({ message }) {
-        return res.status(500).json({ message });
+    if (!institution) {
+      return res.status(404).json({ message: "Instituição não encontrada." });
     }
+
+    return res
+      .status(200)
+      .json({ message: "Instituição deletada com sucesso" });
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
 }
 
 async function getAll(req, res) {
-    try {
-        const institutions = await Institution.find();
-        return res.status(200).json(institutions);
-    } catch ({ message }) {
-        return res.status(500).json({ message });
-    }
+  try {
+    const name = req.query.name;
+    let results = name
+      ? await Institution.find({
+          name: { $regex: `${name}.*`, $options: "i" },
+        }).sort({ name: 1 })
+      : await Institution.find({}).sort({ name: 1 });
+
+    // Paginação
+    const page = parseInt(req.query.page) || 0;
+
+    const limit = 10;
+
+    const startIndex = page * limit;
+
+    const endIndex = (page + 1) * limit;
+
+    const paginatedResults = results.slice(startIndex, endIndex);
+
+    var totalitens = results.length;
+
+    return res.status(200).json({ paginatedResults, totalitens });
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
 }
 
 async function getById(req, res) {
-    try {
-        const institution = await Institution.findById(req.params.id);
-        if (!institution) {
-            return res.status(404).json({ message: 'Instituição não encontrada' });
-        }
-
-        return res.status(200).json(institution);
-    } catch ({ message }) {
-        return res.status(500).json({ message });
+  try {
+    const institution = await Institution.findById(req.params.id);
+    if (!institution) {
+      return res.status(404).json({ message: "Instituição não encontrada" });
     }
+
+    return res.status(200).json(institution);
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
 }
 
 export default {
-    create,
-    update,
-    remove,
-    getAll,
-    getById,
+  create,
+  update,
+  remove,
+  getAll,
+  getById,
 };
--- src/models/Institution.js
+++ src/models/Institution.js
@@ -1,45 +1,50 @@
-import mongoose, { Schema, model } from 'mongoose';
-import Joi from 'joi';
-import { Address, addressRules } from './Address';
-import { Event, eventRules } from './Event';
-import { ContactDetails, contactDetailsRules } from './ContactDetails';
+import mongoose, { Schema, model } from "mongoose";
+import Joi from "joi";
+import { Address, addressRules } from "./Address";
+import { Event, eventRules } from "./Event";
+import { ContactDetails, contactDetailsRules } from "./ContactDetails";
 
 const InstitutionSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: Address,
-            required: true,
-        },
-        information: {
-            type: ContactDetails,
-            required: false,
-        },
-        dailyEvents: {
-            type: Event,
-            required: true,
-        },
-        subscribed: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-                required: false,
-            },
-        ],
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timeStamps: true, discriminatorKey: 'role' }
+    manager: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: Address,
+      required: true,
+    },
+    information: {
+      type: ContactDetails,
+      required: false,
+    },
+    dailyEvents: {
+      type: Event,
+      required: true,
+    },
+    subscribed: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: false,
+      },
+    ],
+  },
+  { timeStamps: true, discriminatorKey: "role" }
 );
 
-const Institution = model('Institution', InstitutionSchema);
+const Institution = model("Institution", InstitutionSchema);
 
 const intitutionRules = Joi.object({
-    name: Joi.string().max(100).required(),
-    address: addressRules.required(),
-    information: contactDetailsRules.required(),
-    dailyEvents: eventRules.required(),
+  name: Joi.string().max(100).required(),
+  manager: Joi.string().max(100).required(),
+  address: addressRules.required(),
+  information: contactDetailsRules.required(),
+  dailyEvents: eventRules.required(),
 });
 
 export { Institution, intitutionRules };
